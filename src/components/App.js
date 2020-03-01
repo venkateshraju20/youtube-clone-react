929e6901d538394bd740be4cc34bda1de65c84e5
Venkatesh Raju <venkatesh.raju20@gmail.com>
2020-03-01 22:15:16 +0530

    Add files

---
+++ src/components/App.js
@@ -0,0 +1,50 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+import youtube from "../api/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
+
+class App extends React.Component {
+  state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit("cars");
+  }
+
+  onTermSubmit = async term => {
+    var response = await youtube.get("/search", {
+      params: {
+        q: term
+      }
+    });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
+  };
+
+  onVideoSelect = video => {
+    this.setState({ selectedVideo: video });
+  };
+
+  render() {
+    const { videos, selectedVideo } = this.state;
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList onVideoSelect={this.onVideoSelect} videos={videos} />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ youtube.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const KEY = {youtube_api_key};
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY
+  }
+});
